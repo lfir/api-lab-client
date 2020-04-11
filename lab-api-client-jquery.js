3d2b6e4ed339b8168220ff2034a4b2bf8e5a253f
Leandro <ltee885@gmail.com>
2020-04-11 23:21:12 +0000

    Add new file

---
+++ lab-api-client-jquery.js
@@ -0,0 +1,16 @@
+// Register page visit.
+$(document).ready(function() {
+    var host = window.location.hostname;
+    var path = window.location.pathname;
+    if (host && path) {
+        var url = 'https://l086.herokuapp.com/api/newpageview';
+        var data = { host: host, path: path };
+        $.get('https://ifconfig.me/ip', function(ip) {
+            if (ip) {
+                data.ip = ip;
+            }
+            //console.log(data);
+            $.post(url, data);
+        });
+    }
+});
