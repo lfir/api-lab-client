7433be6e54406fd853de634855176ebbfb54181e
Leandro <ltee885@gmail.com>
2020-08-22 02:03:51 -0300

    Use Fetch API

--- public/js/jquery-client.js
+++ public/js/jquery-client.js
@@ -1,16 +1,25 @@
-// Register page visit.
-$(document).ready(function() {
-    var host = window.location.hostname;
-    var path = window.location.pathname;
-    if (host && path) {
-        var url = 'https://l086.herokuapp.com/api/newpageview';
-        var data = { host: host, path: path };
-        $.get('https://ifconfig.me/ip', function(ip) {
-            if (ip) {
-                data.ip = ip;
-            }
-            //console.log(data);
-            $.post(url, data);
-        });
-    }
-});
+'use strict';
+
+// Register a page visit event.
+let host = window.location.hostname;
+let path = window.location.pathname;
+if (host && path) sendPageVisitInfo();
+
+function sendPageVisitInfo() {
+  let apiUrl = 'https://l086.herokuapp.com/api/newpageview';
+  let data = { host: host, path: path };
+  let postOptions = { method: 'POST', headers: {'Content-Type': 'application/json'} };
+
+  fetch('https://ifconfig.me/ip')
+    .then(response => response.text())
+    .then(ip => {
+      if (ip) data.ip = ip; 
+      // console.log('IP:', ip);
+      postOptions.body = JSON.stringify(data);
+      fetch(apiUrl, postOptions)
+        // .then(response => response.json())
+        // .then(record => console.log('POST Success:', record))
+        .catch(error => console.error('POST Error:', error));
+    })
+    .catch(error => console.error('GET IP Error:', error));
+}
